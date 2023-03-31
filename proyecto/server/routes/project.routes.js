f2999050720a2972c4e877812d3c641af21c7f46
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-03-31 11:56:28 -0400

    server: GET para realizar pruebas de conexion

--- proyecto/server/routes/project.routes.js
+++ proyecto/server/routes/project.routes.js
@@ -9,4 +9,7 @@ module.exports = (app) => {
 	app.get("/api/getProjects", getProjects);
 	app.put("/api/updateProject/:id", updateProject);
 	app.delete("/api/deleteProject/:id", deleteProject);
+	app.get("/api/test", (req, res) => {
+		res.send("Test exitoso");
+	})
 }
